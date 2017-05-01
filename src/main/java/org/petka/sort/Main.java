45d3d53a6fe70d753ab9113c0392831f4b886be1
petka <petar.georgiev@gmail.com>
2017-05-01 18:11:01 +0300

    Java 8 lambdas implementation.

--- src/main/java/org/petka/sort/Main.java
+++ src/main/java/org/petka/sort/Main.java
@@ -2,57 +2,64 @@ package org.petka.sort;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Main {
 
     private static Random random = new Random();
 
+    private static Function<List<Long>, Long> getNext = (List<Long> a) -> a.remove(0);
+
+    private static Predicate<List<Long>> hasNext = (List<Long> a) -> a.size() != 0;
+
+    private static Function<List<Long>, Long> checkNext = (List<Long> a) -> a.get(0);
+
+    private static Comparator<PriorityQMergeSort<List<Long>, Long>> comparator =
+            (PriorityQMergeSort<List<Long>, Long> o1, PriorityQMergeSort<List<Long>, Long> o2) -> o1.checkNext().compareTo(o2.checkNext());
+
     public static void main(String[] args) {
 
-        PriorityQMergeSorter<ListElement<List<Long>, Long>, List<Long>, Long> prioritySort = new PriorityQMergeSorter<>();
+        PriorityQMergeSorter<PriorityQMergeSort<List<Long>, Long>, List<Long>, Long> prioritySort = new PriorityQMergeSorter<>();
 
-        prioritySort.add(createElement());
-        prioritySort.add(createElement());
-        prioritySort.add(createElement());
-        prioritySort.add(createElement());
+        PriorityQMergeSort<List<Long>, Long> list1 = createWrapper();
 
-        List<Long> list = new ArrayList<>();
-        long time = System.currentTimeMillis();
+        PriorityQMergeSort<List<Long>, Long> list2 = createWrapper();
+
+        PriorityQMergeSort<List<Long>, Long> list3 = createWrapper();
+
+        PriorityQMergeSort<List<Long>, Long> list4 = createWrapper();
+
+        prioritySort.add(list1);
+        prioritySort.add(list2);
+        prioritySort.add(list3);
+        prioritySort.add(list4);
+
+        List<Long> resultList = new ArrayList<>();
         while (prioritySort.hasElement()) {
-            list.add(prioritySort.get());
+            resultList.add(prioritySort.get());
         }
-        time = System.currentTimeMillis() - time;
-        System.out.println(list);
-        System.out.println("Execution time: " + time);
-
-        List<String> listOne = new ArrayList<>();
-        listOne.add("Petka");
-        listOne.add("One");
-
-        List<String> listTwo = new ArrayList<>();
-        listTwo.add("Petka");
-        listTwo.add("Petkaa");
-        listTwo.add("Aloha");
-
-        Collections.sort(listOne);
-        Collections.sort(listTwo);
-
-        PriorityQMergeSorter<PriorityQMergeSort<List<String>, String>, List<String>, String> queueTwo = new PriorityQMergeSorter<>();
-        queueTwo.add(new ListElement<List<String>, String>(listOne));
-        queueTwo.add(new ListElement<List<String>, String>(listTwo));
-
-        while (queueTwo.hasElement()) {
-            System.out.println(queueTwo.get());
-        }
+        System.out.println(resultList);
 
     }
 
-    private static ListElement<List<Long>, Long> createElement() {
+    private static PriorityQMergeSort<List<Long>, Long> createWrapper() {
+        PriorityQMergeSort<List<Long>, Long> wrapper = new PriorityQMergeSort<List<Long>, Long>(createList());
+        wrapper.setCheckNext(checkNext);
+        wrapper.setComparator(comparator);
+        wrapper.setGetNext(getNext);
+        wrapper.setHasNext(hasNext);
+
+        return wrapper;
+    }
+
+    private static List<Long> createList() {
         List<Long> list = new ArrayList<>();
-        fillList(list,random.nextInt(100));
-        return new ListElement<List<Long>, Long>(list);
+        fillList(list, random.nextInt(100));
+        return list;
     }
 
     private static void fillList(List<Long> list, int size) {
--- src/main/java/org/petka/sort/PriorityQMergeSort.java
+++ src/main/java/org/petka/sort/PriorityQMergeSort.java
@@ -1,5 +1,9 @@
 package org.petka.sort;
 
+import java.util.Comparator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 /**
  * 
  * This class wraps sorted collection <b>sources</b> which later will be merged by PriorityQMergeSorter.
@@ -9,34 +13,73 @@ package org.petka.sort;
  * @param <T>
  * @param <E>
  */
-public abstract class PriorityQMergeSort<T, E> implements Comparable<PriorityQMergeSort<T, E>> {
+public class PriorityQMergeSort<T, E> implements Comparable<PriorityQMergeSort<T, E>> {
 
     /**
      * This is the source of the sorted collection
      */
-    protected T source;
+    private T source;
+
+    private Function<T, E> getNext;
+
+    private Predicate<T> hasNext;
+
+    private Function<T, E> checkNext;
+
+    private Comparator<PriorityQMergeSort<T, E>> comparator;
+
+    public PriorityQMergeSort(T source) {
+        this.source = source;
+    }
+
+    public void setGetNext(Function<T, E> getNext) {
+        this.getNext = getNext;
+    }
+
+    public void setHasNext(Predicate<T> hasNext) {
+        this.hasNext = hasNext;
+    }
+
+    public void setCheckNext(Function<T, E> checkNext) {
+        this.checkNext = checkNext;
+    }
+
+    public void setComparator(Comparator<PriorityQMergeSort<T, E>> comparator) {
+        this.comparator = comparator;
+    }
 
     /**
      * Get next element from the sorted collection.
      * 
      * @return <E>
      */
-    public abstract E getNext();
+    public E getNext() {
+        return this.getNext.apply(source);
+
+    }
 
     /**
      * Check if there is another element in collection
      * 
      * @return boolean
      */
-    public abstract boolean hasNext();
+    public boolean hasNext() {
+        return hasNext.test(source);
+
+    }
 
     /**
      * Get next element from collection in order to compare it.
      * 
      * @return <E>
      */
-    public abstract E checkNext();
+    public E checkNext() {
+        return this.checkNext.apply(source);
+    }
 
     @Override
-    public abstract int compareTo(PriorityQMergeSort<T, E> o);
+    public int compareTo(PriorityQMergeSort<T, E> o) {
+        return comparator.compare(this, o);
+
+    }
 }
